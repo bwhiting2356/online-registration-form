a031b4bd2bfaff4c5d33b3006085eb186aa0f813
Brendan Whiting <brendan.whiting@me.com>
2016-02-02 00:42:26 -0500

    fixed issue with margin

--- js/select_dropdown.js
+++ js/select_dropdown.js
@@ -10,75 +10,64 @@ function tamingselect()
 	var ts_triggeroff='trigger';			// class for the inactive trigger link
 	var ts_dropdownclosed='dropdownhidden'; // closed dropdown
 	var ts_dropdownopen='dropdownvisible';	// open dropdown
-	var normal='normal';
-	var adjust='adjust';
-/*
-	Turn all selects into DOM dropdowns
-*/
+	var normal='normal';					// normal margin for zipcode
+	var adjust='adjust';					// adjust so that dropdown doesn't displace zipcode
+
 	var count=0;
 	var toreplace=new Array();
 	var sels=document.getElementsByTagName('select');
-	for(var i=0;i<sels.length;i++){
-		if (ts_check(sels[i],ts_selectclass))
-		{
-			var hiddenfield=document.createElement('input');
-			hiddenfield.name=sels[i].name;
-			hiddenfield.type='hidden';
-			hiddenfield.id=sels[i].id;
-			hiddenfield.value=sels[i].options[0].value;
-			sels[i].parentNode.insertBefore(hiddenfield,sels[i])
 
-			var trigger=document.createElement('a');
-			ts_addclass(trigger,ts_triggeroff);
-			trigger.href='#';
+	var select_dropdown=document.getElementById('select-dropdown');
 
-			var zipcode=document.getElementById('zipcode-label');
+	var hiddenfield=document.createElement('input');
+	hiddenfield.name=select_dropdown.name;
+	hiddenfield.type='hidden';
+	hiddenfield.id=select_dropdown.id;
+	hiddenfield.value=select_dropdown.options[0].value;
+	select_dropdown.parentNode.insertBefore(hiddenfield,select_dropdown)
 
-			trigger.onclick=function(){
-				ts_swapclass(this,ts_triggeroff,ts_triggeron)
-				ts_swapclass(this.parentNode.getElementsByTagName('ul')[0],ts_dropdownclosed,ts_dropdownopen);
+	var trigger=document.createElement('a');
+	ts_addclass(trigger,ts_triggeroff);
+	trigger.href='#';
 
-				ts_swapclass(zipcode, normal, adjust);
+	var zipcode=document.getElementById('zipcode-label');
 
-				return false;
-			}
-
-			trigger.appendChild(document.createTextNode(sels[i].options[0].text));
-			sels[i].parentNode.insertBefore(trigger,sels[i]);
-			var replaceUL=document.createElement('ul');
-			for(var j=0;j<sels[i].getElementsByTagName('option').length;j++)
-			{
-				var newli=document.createElement('li');
-				var newa=document.createElement('a');
-				newli.v=sels[i].getElementsByTagName('option')[j].value;
-				newli.elm=hiddenfield;
-				newli.istrigger=trigger;
-				newa.href='#';
-				newa.appendChild(document.createTextNode(
-				sels[i].getElementsByTagName('option')[j].text));
-				newli.onclick=function(){ 
-					this.elm.value=this.v;
-					ts_swapclass(this.istrigger,ts_triggeron,ts_triggeroff);
-					ts_swapclass(this.parentNode,ts_dropdownopen,ts_dropdownclosed)
-					this.istrigger.firstChild.nodeValue=this.firstChild.firstChild.nodeValue;
-					return false;
-				}
-				newli.appendChild(newa);
-				replaceUL.appendChild(newli);
-			}
-			ts_addclass(replaceUL,ts_dropdownclosed);
-			var div=document.createElement('div');
-			div.appendChild(replaceUL);
-			ts_addclass(div,ts_boxclass);
-			sels[i].parentNode.insertBefore(div,sels[i])
-			toreplace[count]=sels[i];
-			count++;
-		}
+	trigger.onclick=function(){
+		ts_swapclass(this,ts_triggeroff,ts_triggeron)
+		ts_swapclass(this.parentNode.getElementsByTagName('ul')[0],ts_dropdownclosed,ts_dropdownopen);
+		ts_swapclass(zipcode, normal, adjust);
+		return false;
 	}
-	
-/*
-	Turn all ULs with the class defined above into dropdown navigations
-*/	
+
+	trigger.appendChild(document.createTextNode(select_dropdown.options[0].text));
+	select_dropdown.parentNode.insertBefore(trigger,select_dropdown);
+	var replaceUL=document.createElement('ul');
+	for(var j=0;j<select_dropdown.getElementsByTagName('option').length;j++)
+	{
+		var newli=document.createElement('li');
+		var newa=document.createElement('a');
+		newli.v=select_dropdown.getElementsByTagName('option')[j].value;
+		newli.elm=hiddenfield;
+		newli.istrigger=trigger;
+		newa.href='#';
+		newa.appendChild(document.createTextNode(
+		select_dropdown.getElementsByTagName('option')[j].text));
+		newli.onclick=function(){ 
+			this.elm.value=this.v;
+			ts_swapclass(this.istrigger,ts_triggeron,ts_triggeroff);
+			ts_swapclass(this.parentNode,ts_dropdownopen,ts_dropdownclosed);
+			ts_swapclass(zipcode, normal, adjust);
+			this.istrigger.firstChild.nodeValue=this.firstChild.firstChild.nodeValue;
+			return false;
+		}
+		newli.appendChild(newa);
+		replaceUL.appendChild(newli);
+	}
+	ts_addclass(replaceUL,ts_dropdownclosed);
+	var div=document.createElement('div');
+	div.appendChild(replaceUL);
+	ts_addclass(div,ts_boxclass);
+	select_dropdown.parentNode.insertBefore(div,select_dropdown)
 
 	var uls=document.getElementsByTagName('ul');
 	for(var i=0;i<uls.length;i++)
@@ -104,9 +93,9 @@ function tamingselect()
 			count++;
 		}
 	}
-	for(i=0;i<count;i++){
-		toreplace[i].parentNode.removeChild(toreplace[i]);
-	}
+
+	select_dropdown.parentNode.removeChild(select_dropdown);
+
 	function ts_check(o,c)
 	{
 	 	return new RegExp('\\b'+c+'\\b').test(o.className);
